d402e2089e59b2ad19fd1ccbe1dc999adeab0e53
evilbob <spam3@360pc.com>
2026-09-04 01:50:33 +0000

    Revert "bcport(auto): fix NeighbourTileCache.java (3274->3270 errors)"
    This reverts commit ea56bd87b192f19646b9ff99a1dc2656f6200349.

--- common/buildcraft/lib/cache/NeighbourTileCache.java
+++ common/buildcraft/lib/cache/NeighbourTileCache.java
@@ -49,12 +49,12 @@ public class NeighbourTileCache implements ITileCache {
 
     private boolean canUseCache() {
         Level w = tile.getLevel();
-        if (!tile.isValidBlockState(tile.getBlockState()) || w == null) {
+        if (tile.isInvalid() || w == null) {
             return false;
         }
         BlockPos tPos = tile.getBlockPos();
         if (!tPos.equals(lastSeenTilePos)) {
-            lastSeenTilePos = tPos;
+            lastSeenTilePos = tPos.toImmutable();
             cachedTiles.clear();
         }
         if (!w.isLoaded(lastSeenTilePos)) {
@@ -76,7 +76,7 @@ public class NeighbourTileCache implements ITileCache {
         WeakReference<BlockEntity> ref = cachedTiles.get(offset);
         if (ref != null) {
             BlockEntity oTile = ref.get();
-            if (oTile == null || !oTile.isValidBlockState(oTile.getBlockState())) {
+            if (oTile == null || oTile.isInvalid()) {
                 cachedTiles.remove(offset);
             } else {
                 Level w = tile.getLevel();
@@ -97,7 +97,7 @@ public class NeighbourTileCache implements ITileCache {
             chunk = ChunkUtil.getChunk(tile.getLevel(), offsetPos, true);
         }
         BlockState state = chunk.getBlockState(offsetPos);
-        if (!state.hasBlockEntity()) {
+        if (!state.getBlock().hasTileEntity(state)) {
             // Optimisation: world.getTileEntity can be slow (as it potentially iterates through a long list)
             // so just check to make sure the target block might actually have a tile entity
             return new TileCacheRet(null);
